9dda7fceb903d0cccbe833326a78cef03cdb25df
appenjan <69844980+appenjan@users.noreply.github.com>
2020-08-18 18:24:59 +0200

    Update get-protected-content.js

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -3,23 +3,18 @@ const content = {
     src:
       'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'corgi in the park with a sunset in the background',
-    message: 'To view this content, you need to create an account!',
     allowedRoles: ['free', 'pro', 'premium'],
   },
   pro: {
     src:
       'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'close-up of a corgi with its tongue hanging out',
-    message:
-      'This is protected content! It’s only available if you have a pro plan or higher.',
     allowedRoles: ['pro', 'premium'],
   },
   premium: {
     src:
       'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'corgi in a tent with string lights in the foreground',
-    message:
-      'This is protected content! It’s only available if you have the premium plan.',
     allowedRoles: ['premium'],
   },
 };
